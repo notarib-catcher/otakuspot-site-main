d55f62a5fcba6cd9d0ec90a7985ab8790bf1d610
ribcatcher <15999680+notarib-catcher@users.noreply.github.com>
2023-07-23 20:56:12 +0530

    Initial commit with a slightly restyled placeholder site

---
+++ src/routes/(public)/+layout.svelte
@@ -0,0 +1,48 @@
+<script>
+    import bgimg from "../../assets/bg.webp"
+</script>
+
+<img src={bgimg} class="min-h-screen w-screen object-cover fixed -z-50 brightness-[20%] bgfadein">
+<div class="fadeinafterbg">
+    <slot />
+</div>
+
+<style>
+
+    .bgfadein{
+        animation-name: fadeinbg;
+        animation-fill-mode: forwards;
+        animation-duration: 500ms;
+    }
+
+    @keyframes fadeinbg{
+        0%{
+            filter: invert(100%) brightness(40%);
+        }
+
+        100%{
+            filter: invert(0) brightness(20%);
+        }
+    }
+
+    .fadeinafterbg{
+        animation-name: fadeinpostbg;
+        animation-fill-mode: forwards;
+        animation-duration: 1000ms;
+    }
+
+    @keyframes fadeinpostbg{
+        0%{
+            opacity: 0;
+            transform: translateY(-10px);
+        }
+        50%{
+            opacity: 0;
+            transform: translateY(-10px);
+        }
+        100%{
+            opacity: 1;
+            transform: translateY(0);
+        }
+    }
+</style>
