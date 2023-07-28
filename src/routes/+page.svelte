1bdfee54a73400e5e8f555423f77bbe695b82110
ribcatcher <15999680+notarib-catcher@users.noreply.github.com>
2023-07-29 00:07:37 +0530

    Added a background - we are cultured

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+    import bg from "../assets/bg.webp"
     import {signIn} from "@auth/sveltekit/client";
 
     import { browser } from '$app/environment';
@@ -12,22 +13,27 @@
         window.history.pushState({}, document.title, window.location.pathname);
     }
 </script>
-<div class=" h-screen w-screen bg-black overflow-hidden flex flex-col items-center align-middle justify-center">
-    <div class = "absolute top-0 w-screen h-fit z-20 pointer-events-none">
-        {#if isUnauth}
-            <div class=" backdrop-blur-xl bg-red-700 bg-clip-text text-transparent text-2xl font-semibold h-[50px] w-fit p-2 font-mono rounded-lg mx-auto ModalPopIn backgroundpan">
-                Unauthorized
-            </div>
-        {/if}
+
+<div>
+    <div class=" h-screen w-screen bg-transparent overflow-hidden flex flex-col items-center align-middle justify-center">
+        <div class = "absolute top-0 w-screen h-fit z-20 pointer-events-none">
+            {#if isUnauth}
+                <div class=" backdrop-blur-xl bg-red-700 bg-clip-text text-transparent text-2xl font-semibold h-[50px] w-fit p-2 font-mono rounded-lg mx-auto ModalPopIn backgroundpan">
+                    Unauthorized
+                </div>
+            {/if}
+        </div>
+        <div class=" h-[200px] w-[300px] bg-white bg-opacity-20 rounded-3xl border-white border-[2px] flex flex-col items-center align-middle justify-center">
+            <button class=" movedown fontpoppins h-[50px] min-w-[150px] max-w-fit duration-100 hover:border-b-[4px] text-white border-white" on:click={() => {signIn("google",{callbackUrl: "/postlogin"})}}>
+                Login with Google
+            </button>
+        </div>
     </div>
-    <div class=" h-[200px] w-[300px] bg-white bg-opacity-20 rounded-3xl border-white border-[2px] flex flex-col items-center align-middle justify-center">
-        <button class=" movedown fontpoppins h-[50px] min-w-[150px] max-w-fit duration-100 hover:border-b-[4px] text-white border-white" on:click={() => {signIn("google",{callbackUrl: "/postlogin"})}}>
-            Login with Google
-        </button>
-    </div>
+    <img src={bg} class=" object-cover min-h-screen min-w-full fixed top-0 left-0 -z-10" style="filter: brightness(20%)">
 </div>
 
 <style>
+
     .movedown{
         transform: translateY(0);
     }
@@ -41,6 +47,9 @@
         animation-iteration-count: 1;
         animation-fill-mode: forward;
     }
+
+
+
     @keyframes login{
         0%{
             transform: translateY(-55px);
